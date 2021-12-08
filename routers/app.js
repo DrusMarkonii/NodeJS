b25855d76bd2414ab950d81faff9473bce43cbbd
MakarevichAndrey <78726417+DrusMarkonii@users.noreply.github.com>
2021-12-08 14:30:17 +0300

    Add files via upload

--- routers/app.js
+++ routers/app.js
@@ -1,6 +1,8 @@
 import http from 'http';
 import { RoutersHandler } from './core/RoutersHandler.js';
 import { router } from './routes/main.js';
+import {initDatabase} from './database/initDatabase.js';
+
 const PORT = 5000;
 
 const RoutesHandler = new RoutersHandler(router);
@@ -11,6 +13,9 @@ http.createServer( (req, res) => {
     
     RoutesHandler.handleRequest(req, res)
 
-}).listen(PORT, () => {
-    console.log(`Server was started on PORT ${PORT}....`)
+}).listen(PORT, async () => {
+
+    console.log(`Server was started on PORT ${PORT}....`);
+    await initDatabase();
+
 })
